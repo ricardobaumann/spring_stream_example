64f4019e9bd9676e04430dd292901c78113759bb
Ricardo Baumann <gemini.richard@gmail.com>
2017-07-14 14:42:42 +0200

    ignite support

--- lookup/src/main/java/contentplatform/CacheRepo.java
+++ lookup/src/main/java/contentplatform/CacheRepo.java
@@ -1,22 +1,23 @@
 package contentplatform;
 
+import org.apache.ignite.IgniteCache;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CacheRepo {
 
-    private final ContentUnitRepo contentUnitRepo;
+    private final IgniteCache<String, String> igniteCache;
 
-    CacheRepo(final ContentUnitRepo contentUnitRepo) {
-        this.contentUnitRepo = contentUnitRepo;
+    CacheRepo(final IgniteCache<String, String> igniteCache) {
+        this.igniteCache = igniteCache;
     }
 
     void save(final ContentUnit contentUnit) {
-        contentUnitRepo.save(contentUnit);
+        igniteCache.put(contentUnit.getId(), contentUnit.getBody());
     }
 
     ContentUnit get(final String id) {
-        return contentUnitRepo.findOne(id);
+        return new ContentUnit(id, igniteCache.get(id));
     }
 
 }
--- lookup/src/main/java/contentplatform/ContentUnitRepo.java
+++
@@ -1,8 +0,0 @@
-package contentplatform;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContentUnitRepo extends CrudRepository<ContentUnit, String> {
-}
